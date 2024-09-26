883c232db3c11fed417f69b4a4a44285ed388df5
prince <pr63547890@gecg28.ac.in>
2024-09-26 15:21:29 +0530

    add login in home

--- frontend-react/src/pages/home/page.tsx
+++ frontend-react/src/pages/home/page.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       Home
+      <Link to={"login"}>Login</Link>
     </div>
   )
 }
